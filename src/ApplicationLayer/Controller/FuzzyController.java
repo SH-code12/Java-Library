262f97bcf1baf729792e9a89adf6f71aeee299a9
Shahd Elnasssag <shahdelnassag@gmail.com>
2025-11-02 17:39:15 +0200

    Edit Structure

---
+++ src/ApplicationLayer/Controller/FuzzyController.java
@@ -0,0 +1,9 @@
+package ApplicationLayer.Controller;
+
+public class FuzzyController {
+
+    public static void main(String[] args) {
+        System.out.println("Sorry, Not added Yet.");
+
+    }
+    }
