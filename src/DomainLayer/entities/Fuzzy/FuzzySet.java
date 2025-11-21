a59c0d612447dee1093db3bf77b768657dc80470
Shahd Elnasssag <shahdelnassag@gmail.com>
2025-11-21 17:21:23 +0200

    Add LinguisticVariable and FuzzySet

--- src/DomainLayer/entities/Fuzzy/FuzzySet.java
+++ src/DomainLayer/entities/Fuzzy/FuzzySet.java
@@ -1,4 +1,17 @@
 package DomainLayer.entities.Fuzzy;
 
+import DomainLayer.interfaces.Fuzzy.MembershipFunction;
+
 public class FuzzySet {
+    private final String name;
+    private final MembershipFunction mf;
+    public FuzzySet(String name, MembershipFunction mf){
+        this.name=name;this.mf=mf;
+    }
+    public String getName(){
+        return name;
+    }
+    public MembershipFunction getMf(){
+        return mf;
+    }
 }
--- src/DomainLayer/entities/Fuzzy/LinguisticVariable.java
+++ src/DomainLayer/entities/Fuzzy/LinguisticVariable.java
@@ -1,4 +1,24 @@
 package DomainLayer.entities.Fuzzy;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class LinguisticVariable {
+    private final String name;
+    private final double min, max;
+    private final Map<String, FuzzySet> fuzzySets = new LinkedHashMap<>();
+    public LinguisticVariable(String name,double min,double max){
+        this.name=name;
+        this.min=min;
+        this.max=max;}
+    public String getName(){
+        return name;}
+    public double getMin(){
+        return min;}
+    public double getMax(){
+        return max;}
+    public void addFuzzySet(FuzzySet s){
+        fuzzySets.put(s.getName(), s);}
+    public Map<String,FuzzySet> getFuzzySets(){
+        return fuzzySets;}
 }
