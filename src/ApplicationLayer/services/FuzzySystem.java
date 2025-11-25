be9486b92ac31af44f3e7f0a12c31e3e649af56d
MalakHisham121 <malak.hesham10011@gmail.com>
2025-11-25 22:36:31 +0200

    do some work in usecase part and FuzzySystem class

--- src/ApplicationLayer/services/FuzzySystem.java
+++ src/ApplicationLayer/services/FuzzySystem.java
@@ -1,20 +1,48 @@
 package ApplicationLayer.services;
 
 import DomainLayer.entities.Fuzzy.FuzzyRule;
+import DomainLayer.entities.Fuzzy.LinguisticVariable;
+import DomainLayer.entities.Fuzzy.RuleBase;
 import DomainLayer.interfaces.Fuzzy.DefuzzificationStrategy;
 import DomainLayer.interfaces.Fuzzy.InferenceStrategy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FuzzySystem {
-    List<String> Inputvariables;
-    List<String> Outputvariables;
-    List<FuzzyRule> rules;
-    InferenceStrategy inferenceStrategy;
-    DefuzzificationStrategy defuzzificationStrategy;
-    public void setInferenceEngine(InferenceStrategy type){}
-    public void setDefuzzifier(DefuzzificationStrategy type){}
-    public void addInputVariable(){}
+    private Map<String,LinguisticVariable> Inputvariables;
+    private LinguisticVariable Outputvariables;
+    private List<FuzzyRule> rules;
+    private InferenceStrategy inferenceStrategy;
+    private DefuzzificationStrategy defuzzificationStrategy;
 
-    public void createRule(){}
+
+    public FuzzySystem() {
+        this.Inputvariables = new HashMap<>();
+        this.rules = new ArrayList<>();
+    }
+
+
+    public void addInputVariable(LinguisticVariable variable) {
+        this.Inputvariables.put(variable.getName(), variable);
+    }
+    public void setOutputVariable(LinguisticVariable variable) {
+        this.Outputvariables = variable;
+    }
+    public void addRule(FuzzyRule rule) {
+        this.rules.add(rule);
+    }
+
+    public void setInferenceEngine(InferenceStrategy strategy) {
+        this.inferenceStrategy = strategy;
+    }
+
+    public void setDefuzzifier(DefuzzificationStrategy strategy) {
+        this.defuzzificationStrategy = strategy;
+    }
+    // Following fn need to be implemented
+    public double evaluate(Map<String, Double> crispInputs){}
+
 }
--- src/PresentationLayer/Fuzzy/StockMarketModel.java
+++ src/PresentationLayer/Fuzzy/StockMarketModel.java
@@ -1,20 +1,47 @@
 package PresentationLayer.Fuzzy;
 
 import ApplicationLayer.services.FuzzySystem;
+import DomainLayer.entities.Fuzzy.FuzzyRule;
+import DomainLayer.entities.Fuzzy.FuzzySet;
+import DomainLayer.entities.Fuzzy.LinguisticVariable;
 import InfrastructureLayer.Fuzzy.membership.Gaussian;
 import InfrastructureLayer.Fuzzy.membership.Trapezoidal;
 import InfrastructureLayer.Fuzzy.membership.Triangular;
 
 public class StockMarketModel {
-    int priceTrend,volume,volatility;
-    public  void buildStock(){
+    private FuzzySystem system;
+
+    public StockMarketModel() {
+        this.system = new FuzzySystem();
+        buildStock();
+    }
+
+    private  void buildStock(){
+        LinguisticVariable trend = new LinguisticVariable("PriceTrend",-10,10);
+        trend.addFuzzySet(new FuzzySet("Negative", new Trapezoidal(-15, -10, -2, 0)));
+        trend.addFuzzySet(new FuzzySet("Neutral", new Triangular(-2, 0, 2)));
+        trend.addFuzzySet(new FuzzySet("Positive", new Trapezoidal(0, 2, 10, 15)));
+
+        system.addInputVariable(trend);
 
 
-        FuzzySystem system = new FuzzySystem();
-        Trapezoidal Low,High,Stable,Negative,Positive,Risky;
-        Gaussian Medium;
-        Triangular Neutral;
+        LinguisticVariable volume = new LinguisticVariable("Volume", 0, 1000000);
 
-        system.addInputVariable();
+        volume.addFuzzySet(new FuzzySet("Low", new Trapezoidal(0, 0, 200000, 400000)));
+        volume.addFuzzySet(new FuzzySet("Medium", new Gaussian(500000, 100000))); // Mean, Sigma
+        volume.addFuzzySet(new FuzzySet("High", new Trapezoidal(600000, 800000, 1000000, 1000000)));
+
+        system.addInputVariable(volume);
+
+        LinguisticVariable decision = new LinguisticVariable("Decision", 0, 100);
+
+        decision.addFuzzySet(new FuzzySet("Sell", new Trapezoidal(0, 0, 30, 50)));
+        decision.addFuzzySet(new FuzzySet("Hold", new Triangular(40, 50, 60)));
+        decision.addFuzzySet(new FuzzySet("Buy", new Trapezoidal(50, 70, 100, 100)));
+
+        system.setOutputVariable(decision);
+
+        // Define Rules here example rule
+
     }
 }
